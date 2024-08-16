4daafd7a55f3a722a380deea4f426ca1390ae61c
Amina880 <dkalekwa37@gmail.com>
2024-08-16 22:24:35 +0300

    added more styles

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -4,8 +4,8 @@
         padding: '20px', 
         margin: '10px' }}>
             <h2 style={{fontWeight: 'bold'}}>{props.name}</h2>
-            <p style={{fontSize: '20px'}}>Age: {props.age}</p>
-            <p style={{fontSize: '20px'}}>Bio: {props.bio}</p>
+            <p>Age: <span style={{fontSize: '20px'}}> {props.age}</span></p>
+            <p>Bio: <span style={{fontSize: '20px'}} >{props.bio}</span></p>
         </div>
     );
  };
