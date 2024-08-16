8fe2cbda4d5d81aef7a528088a12a01d3e7e6217
Amina880 <dkalekwa37@gmail.com>
2024-08-16 22:27:28 +0300

    add even more styles

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -3,9 +3,9 @@
         <div style={{ border: '1px solid deeppink', 
         padding: '20px', 
         margin: '10px' }}>
-            <h2 style={{fontWeight: 'bold'}}>{props.name}</h2>
-            <p>Age: <span style={{fontSize: '20px'}}> {props.age}</span></p>
-            <p>Bio: <span style={{fontSize: '20px'}} >{props.bio}</span></p>
+            <h2 style={{fontWeight: 'bold', color:'blue'}}>{props.name}</h2>
+            <p>Age: <span style={{fontSize: '20px', fontWeight:'bold'}}> {props.age}</span></p>
+            <p>Bio: <span style={{fontSize: '20px', fontWeight:'bold'}} >{props.bio}</span></p>
         </div>
     );
  };
