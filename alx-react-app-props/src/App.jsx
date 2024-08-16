2b4a0036324e7b68a9e1c360592aba46ddfd29d7
Amina880 <dkalekwa37@gmail.com>
2024-08-16 23:04:22 +0300

    context

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,10 +7,14 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import ProfilePage from './ProfilePage';
+import { UserContext } from './UserContext'
+
 
 
 function App() {
   const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com"}
 
   return (
     <>
@@ -19,6 +23,10 @@ function App() {
     <MainContent />
     <Footer />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <UserContext.Provider value={userData}>
+     <ProfilePage />
+    </UserContext.Provider>
+      
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
