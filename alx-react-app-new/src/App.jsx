a2a760cfd739b0177642fcd03924189edad2497d
Amina880 <dkalekwa37@gmail.com>
2024-08-16 22:37:35 +0300

    added counterr

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 
 function App() {
@@ -19,6 +20,7 @@ function App() {
     <MainContent />
     <Footer />
     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <Counter />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
