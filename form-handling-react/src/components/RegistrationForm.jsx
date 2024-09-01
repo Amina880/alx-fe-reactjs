9fa660e04a6e50f5200d578b481318e670dd5882
Amina880 <dkalekwa37@gmail.com>
2024-09-01 21:07:23 +0300

    change values back

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -21,20 +21,20 @@ const handleSubmit = (e) => {
     <form onSubmit={handleSubmit}>
         <input 
         type='text'
-        name='name'
-        value = {username}
+        name='username'
+        value = {formData.username}
         onChange={handleChange}
         />
         <input 
         type='email'
         name='email'
-        value = {email}
+        value = {formData.email}
         onChange={handleChange}
         />
         <input 
         type='text'
         name='password'
-        value = {password}
+        value = {formData.password}
         onChange={handleChange}
         />
         <button type= "submit">Submit</button>
