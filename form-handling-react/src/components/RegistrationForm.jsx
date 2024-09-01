778c79f171495d18052e45df63cc807ccd6b7277
Amina880 <dkalekwa37@gmail.com>
2024-09-01 22:53:14 +0300

    added validation

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -9,13 +9,29 @@ function RegistrationForm() {
 const [username, setUserName] = useState('')
 const [email, setEmail] = useState('')
 const [password, setPassword] = useState('')
+const [errors, setErrors] = useState({});
 
+const formError ={}; 
 
 const handleSubmit = (e) => {
     e.preventDefault()
+    if (!username){
+      errors.username('Username must be more than 5 characters')
+      return;
+    }
+    if (!email){
+      errors.email('Invalid email')
+      return;
+    }
+    if (!password){
+      errors.password('Password must be more than 5 characters')
+      return;
+    }
+    setErrors(formError)
     console.log(username)
     console.log(email)
     console.log(password)
+
 };
   return (
     <form onSubmit={handleSubmit}>
