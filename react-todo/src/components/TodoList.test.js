68b35d4a973463750a0d2dca152a6d63e60c4cca
Amina880 <dkalekwa37@gmail.com>
2024-09-01 23:51:57 +0300

    last

--- react-todo/src/components/TodoList.test.js
+++ react-todo/src/components/TodoList.test.js
@@ -1,3 +1,4 @@
+import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 import { render, screen, fireEvent } from '@testing-library/react';
 
@@ -7,4 +8,12 @@ test('renders todo list Component', () => {
     //Verify Component Initial Render
     expect (screen.getByText('Eat food')).toBeInTheDocument();
     
+})
+
+// Test to check if the  button works
+test('add task', () => {
+    render(<AddTodoForm />);
+    //Simulate click event
+    fireEvent.click(screen.getByText('Add Task'))
+    expect(screen.getByText('Make bed')).toBeInTheDocument();
 })
