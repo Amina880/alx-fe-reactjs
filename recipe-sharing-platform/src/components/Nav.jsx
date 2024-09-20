aab977dafa16b548b6bca5fc86ba7f379c12357d
Amina880 <dkalekwa37@gmail.com>
2024-09-20 16:38:45 +0300

    chore:added + images

--- recipe-sharing-platform/src/components/Nav.jsx
+++ recipe-sharing-platform/src/components/Nav.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <nav className='text-xl mx-auto max-w-fit italic '>
-        <ul>
-            <li>
+    <nav className='text-xl mx-auto max-w-fit italic flex '>
+        
                 <Link className='p-2 mr-2 bg-slate-100 border-2 border-stone-500' to="/">Home </Link>
-                <Link className='p-2 mr-2 bg-slate-100 border-2 border-stone-500' to="/add-recipe-form"> Add New Recipe</Link>
-            </li>
-        </ul>
+                <Link className='p-2 mr-2 bg-slate-100 rounded-3xl border-2 border-stone-100  ' to="/add-recipe-form">
+                <img className='w-8' src="https://img.icons8.com/ios/50/000000/plus.png" alt="plus"/>
+                </Link>
+            
     </nav>
 
   )
