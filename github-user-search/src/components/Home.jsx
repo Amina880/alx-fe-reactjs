7200c336a2e83c039fe56bb2f1f1b9954836dccb
Amina880 <dkalekwa37@gmail.com>
2024-09-21 23:41:51 +0300

    fix:changed title

--- github-user-search/src/components/Home.jsx
+++ github-user-search/src/components/Home.jsx
@@ -4,7 +4,7 @@ import Search from './Search'
 function Home() {
   return (
     <div>
-      <h1>Hi</h1>
+      <h1 className='md:text-3xl text-2xl p-10 w-fit mx-auto semibold text-slate-600 '>Find A GitHub User</h1>
       <Search />
     </div>
   )
