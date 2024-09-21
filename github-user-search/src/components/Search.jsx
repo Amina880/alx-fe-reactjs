23c006b153f0003c5a76846bdda6a0447d8d179a
Amina880 <dkalekwa37@gmail.com>
2024-09-21 23:44:38 +0300

    feat:add location and min repos

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -5,6 +5,8 @@ import  fetchUserData  from '../services/githubService'
 function Search() {
 
 const [username, setUsername] = useState('')
+const [location, setLocation] = useState('')
+const [minRepos, setMinRepos] = useState('')
 const [userData, setUserData]= useState(null)
 const [loading, setLoading]= useState(false)
 const [errors, setErrors]= useState({})
@@ -12,6 +14,12 @@ const resetForm = () => setUsername('');
 
 const login = (e) => {
     setUsername(e.target.value);
+}
+const loginLocation = (e) => {
+    setLocation(e.target.value);
+}
+const loginMinRepos = (e) => {
+    setMinRepos(e.target.value);
 }
 const onSubmit = async (e) => {
     e.preventDefault();
@@ -26,8 +34,9 @@ const onSubmit = async (e) => {
     setLoading(true)
 
     try {
-      const data = await fetchUserData(username);
-      setUserData(data);
+      const data = await fetchUserData({username, location, minRepos});
+      setUserData(data.items);
+      console.log(userData)
     } 
     catch (error) {
         console.error('Looks like we cant find the user', error);
@@ -48,25 +57,44 @@ const validate = () => {
 
   return (
     <>
+    <div className='flex w-fit mx-auto'>
     <form>
-        <input type="text" placeholder='Enter Username' required onChange={login} />
-        <button type="submit" onClick={onSubmit}>Search</button>
+        <div className=' mx-auto md:p-8 p-6 md:pb-20 pb-14 bg-stone-200 rounded-lg shadow-md'>
+        <input className='w-full mx-auto border-2 p-1 border-grey-700 rounded-lg mt-4' type="text" placeholder='Enter Username' required onChange={login} />
+        <br></br>
+        <br></br>
+        <p className='py-6 font-semibold text-slate-600'>Search by:</p>
+        <input className=' mr-2 border-2 p-1 border-grey-600 rounded' type="text" placeholder='Location' onChange={loginLocation} />
+        <input className='border-2 p-1 border-grey-600 rounded' type="number" placeholder='Number of Repos'  onChange={loginMinRepos} />
+        <br></br>
+        <br></br>
         {errors.username && <p>{errors.username}</p>}
-         </form>
+        <button className='bg-sky-400 text-lg text-slate-600 flex font-semibold px-4 mx-auto p-2 mt-8 rounded-xl hover:shadow-lg hover:bg-sky-300 ' type="submit" onClick={onSubmit}>Search</button>
+        
+        </div>
+    </form>
 
-        {loading ? (<p> Loading...</p>)
-        : errors.username ? (<p>Looks like we cant find the user</p>)
-    
+        {loading ? (<div className='bg-slate-100 px-20 rounded-lg shadow-md'> <p className='md:text-xl text-lg py-12'> Loading...</p></div>)
+        : errors.username ? (<div className='bg-slate-100 md:px-20 px-14 rounded-lg shadow-md' > <p className='md:text-xl text-lg py-12 wrap '>Looks like we cant find the user</p></div>)
         :userData ?
         (
         <div>
-          <h2>{userData.name}</h2>
-          <p>{userData.bio}</p>
-          <img src={userData.avatar_url} alt="Avatar" />
+        {userData.map(user => (
+        <div key={user.id} className='bg-slate-100 px-8 rounded-lg shadow-md'>
+          <h2 className='text-slate-600 md:text-2xl text-xl text-center font-semibold pt-6  '>{user.login}</h2>
+          <p className='text-slate-500 md:text-xl text-lg text-center '>{user.url}</p>
+          <p className='text-slate-500 md:text-xl text-lg text-center '>{user.location}</p>
+          <p className='text-slate-500 md:text-xl text-lg text-center ' >{user.repos_url}</p>
+          <img className='w-52 mx-auto rounded p-4 pt-10' src={user.avatar_url} alt="Avatar" />
         </div>
-      ):(<p>No user found</p>)
+        ))}
+        </div>
+        )
+      :(<p>No user found</p>)
     }
+    </div>
     </>
+    
   )
 }
 
